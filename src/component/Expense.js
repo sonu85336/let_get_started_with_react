4e7c14d5a1e39603b8ca74467c3f8b3c8ee41e1f
sonu85336 <sonukushwaha098@gmail.com>
2022-12-10 13:48:56 +0530

    task7

---
+++ src/component/Expense.js
@@ -0,0 +1,15 @@
+
+function Expenseitem(){
+    return (
+        <div>
+    <h2>Expenseitem </h2>
+    <h6>Food Rs 10</h6>
+    <h6>Petrol Rs 100</h6>
+    <h6> Movies Rs 200</h6>
+    </div>
+    )
+            
+    
+    
+}
+export default Expenseitem;  
