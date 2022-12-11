8ad3fd7d443f14aa33bf68b9c7cdf88529f635c2
sonu85336 <sonukushwaha098@gmail.com>
2022-12-11 12:35:41 +0530

    task9 expense

--- src/component/Expense.js
+++ src/component/Expense.js
@@ -1,25 +1,18 @@
+import ExpenseDate from "./ExpenseDate";
+import ExpesnseDetails from "./ExpenseDetails";
+import ExpenseAmount from "./ExpenseAmount";
+
 import "./Expenseitem.css";
 
-
- 
 function Expenseitem(props) {
   return (
+    <div className="expense-item ">
      
-      <div className="expense-item ">
-         
-        <div className="expense-item__description ">
-          <h2>{props.title} </h2>
-        </div>
-        <div className = "expense-item "><h5>{props.date.toISOString()}</h5> </div>
-        <div className="expense-item__description " >
-            <h5>{props.LocationOfExpenditure}</h5>
-        </div>
-        <div classNmae="expense-item__price"> <h3>Rs{props.amount}</h3></div>
-      </div>
- 
- 
-     
-     
+      < ExpesnseDetails LocationOfExpenditure={props.LocationOfExpenditure}></ExpesnseDetails> 
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <ExpesnseDetails title = {props.title}></ExpesnseDetails>
+      < ExpenseAmount amount={props.amount}></ExpenseAmount>
+    </div>
   );
 }
 export default Expenseitem;
